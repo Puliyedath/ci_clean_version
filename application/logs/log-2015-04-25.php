bd3aa852043237f1cf6b94863461da9447507aad
hareesh <hareesh.puliyedath@gmail.com>
2015-04-24 19:04:07 -0700

    adds files for custom routes

---
+++ application/logs/log-2015-04-25.php
@@ -0,0 +1,202 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2015-04-25 01:56:40 --> Config Class Initialized
+DEBUG - 2015-04-25 01:56:40 --> Hooks Class Initialized
+DEBUG - 2015-04-25 01:56:40 --> Utf8 Class Initialized
+DEBUG - 2015-04-25 01:56:40 --> UTF-8 Support Enabled
+DEBUG - 2015-04-25 01:56:40 --> URI Class Initialized
+DEBUG - 2015-04-25 01:56:40 --> Router Class Initialized
+ERROR - 2015-04-25 01:56:40 --> 404 Page Not Found --> products
+DEBUG - 2015-04-25 01:57:31 --> Config Class Initialized
+DEBUG - 2015-04-25 01:57:31 --> Hooks Class Initialized
+DEBUG - 2015-04-25 01:57:31 --> Utf8 Class Initialized
+DEBUG - 2015-04-25 01:57:31 --> UTF-8 Support Enabled
+DEBUG - 2015-04-25 01:57:31 --> URI Class Initialized
+DEBUG - 2015-04-25 01:57:31 --> Router Class Initialized
+DEBUG - 2015-04-25 01:57:31 --> No URI present. Default controller set.
+DEBUG - 2015-04-25 01:57:31 --> Output Class Initialized
+DEBUG - 2015-04-25 01:57:31 --> Security Class Initialized
+DEBUG - 2015-04-25 01:57:31 --> Input Class Initialized
+DEBUG - 2015-04-25 01:57:31 --> Global POST and COOKIE data sanitized
+DEBUG - 2015-04-25 01:57:31 --> Language Class Initialized
+DEBUG - 2015-04-25 01:57:31 --> Loader Class Initialized
+DEBUG - 2015-04-25 01:57:31 --> Helper loaded: url_helper
+DEBUG - 2015-04-25 01:57:31 --> Database Driver Class Initialized
+ERROR - 2015-04-25 01:57:31 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Users/hareeshudumbath/CIAssignments/ci_clean_version/system/database/drivers/mysql/mysql_driver.php 91
+DEBUG - 2015-04-25 01:57:31 --> Session Class Initialized
+DEBUG - 2015-04-25 01:57:31 --> Helper loaded: string_helper
+DEBUG - 2015-04-25 01:57:31 --> Session routines successfully run
+DEBUG - 2015-04-25 01:57:31 --> Controller Class Initialized
+DEBUG - 2015-04-25 01:57:31 --> Language file loaded: language/english/profiler_lang.php
+DEBUG - 2015-04-25 01:57:31 --> Helper loaded: text_helper
+DEBUG - 2015-04-25 01:57:31 --> Final output sent to browser
+DEBUG - 2015-04-25 01:57:31 --> Total execution time: 0.0321
+DEBUG - 2015-04-25 01:58:03 --> Config Class Initialized
+DEBUG - 2015-04-25 01:58:03 --> Hooks Class Initialized
+DEBUG - 2015-04-25 01:58:03 --> Utf8 Class Initialized
+DEBUG - 2015-04-25 01:58:03 --> UTF-8 Support Enabled
+DEBUG - 2015-04-25 01:58:03 --> URI Class Initialized
+DEBUG - 2015-04-25 01:58:03 --> Router Class Initialized
+DEBUG - 2015-04-25 01:58:03 --> Output Class Initialized
+DEBUG - 2015-04-25 01:58:03 --> Security Class Initialized
+DEBUG - 2015-04-25 01:58:03 --> Input Class Initialized
+DEBUG - 2015-04-25 01:58:03 --> Global POST and COOKIE data sanitized
+DEBUG - 2015-04-25 01:58:03 --> Language Class Initialized
+DEBUG - 2015-04-25 01:58:03 --> Loader Class Initialized
+DEBUG - 2015-04-25 01:58:03 --> Helper loaded: url_helper
+DEBUG - 2015-04-25 01:58:03 --> Database Driver Class Initialized
+ERROR - 2015-04-25 01:58:03 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Users/hareeshudumbath/CIAssignments/ci_clean_version/system/database/drivers/mysql/mysql_driver.php 91
+DEBUG - 2015-04-25 01:58:03 --> Session Class Initialized
+DEBUG - 2015-04-25 01:58:03 --> Helper loaded: string_helper
+DEBUG - 2015-04-25 01:58:03 --> Session routines successfully run
+DEBUG - 2015-04-25 01:58:03 --> Controller Class Initialized
+DEBUG - 2015-04-25 01:58:03 --> Language file loaded: language/english/profiler_lang.php
+DEBUG - 2015-04-25 01:58:04 --> Helper loaded: text_helper
+DEBUG - 2015-04-25 01:58:04 --> Final output sent to browser
+DEBUG - 2015-04-25 01:58:04 --> Total execution time: 0.0310
+DEBUG - 2015-04-25 01:59:01 --> Config Class Initialized
+DEBUG - 2015-04-25 01:59:01 --> Hooks Class Initialized
+DEBUG - 2015-04-25 01:59:01 --> Utf8 Class Initialized
+DEBUG - 2015-04-25 01:59:01 --> UTF-8 Support Enabled
+DEBUG - 2015-04-25 01:59:01 --> URI Class Initialized
+DEBUG - 2015-04-25 01:59:01 --> Router Class Initialized
+DEBUG - 2015-04-25 01:59:01 --> Output Class Initialized
+DEBUG - 2015-04-25 01:59:01 --> Security Class Initialized
+DEBUG - 2015-04-25 01:59:01 --> Input Class Initialized
+DEBUG - 2015-04-25 01:59:01 --> Global POST and COOKIE data sanitized
+DEBUG - 2015-04-25 01:59:01 --> Language Class Initialized
+DEBUG - 2015-04-25 01:59:01 --> Loader Class Initialized
+DEBUG - 2015-04-25 01:59:01 --> Helper loaded: url_helper
+DEBUG - 2015-04-25 01:59:01 --> Database Driver Class Initialized
+ERROR - 2015-04-25 01:59:01 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Users/hareeshudumbath/CIAssignments/ci_clean_version/system/database/drivers/mysql/mysql_driver.php 91
+DEBUG - 2015-04-25 01:59:01 --> Session Class Initialized
+DEBUG - 2015-04-25 01:59:01 --> Helper loaded: string_helper
+DEBUG - 2015-04-25 01:59:01 --> Session routines successfully run
+DEBUG - 2015-04-25 01:59:01 --> Controller Class Initialized
+DEBUG - 2015-04-25 01:59:01 --> Language file loaded: language/english/profiler_lang.php
+DEBUG - 2015-04-25 01:59:01 --> Helper loaded: text_helper
+DEBUG - 2015-04-25 01:59:01 --> Final output sent to browser
+DEBUG - 2015-04-25 01:59:01 --> Total execution time: 0.0317
+DEBUG - 2015-04-25 02:01:37 --> Config Class Initialized
+DEBUG - 2015-04-25 02:01:37 --> Hooks Class Initialized
+DEBUG - 2015-04-25 02:01:37 --> Utf8 Class Initialized
+DEBUG - 2015-04-25 02:01:37 --> UTF-8 Support Enabled
+DEBUG - 2015-04-25 02:01:37 --> URI Class Initialized
+DEBUG - 2015-04-25 02:01:37 --> Router Class Initialized
+DEBUG - 2015-04-25 02:01:37 --> Output Class Initialized
+DEBUG - 2015-04-25 02:01:37 --> Security Class Initialized
+DEBUG - 2015-04-25 02:01:37 --> Input Class Initialized
+DEBUG - 2015-04-25 02:01:37 --> Global POST and COOKIE data sanitized
+DEBUG - 2015-04-25 02:01:37 --> Language Class Initialized
+DEBUG - 2015-04-25 02:01:37 --> Loader Class Initialized
+DEBUG - 2015-04-25 02:01:37 --> Helper loaded: url_helper
+DEBUG - 2015-04-25 02:01:37 --> Database Driver Class Initialized
+ERROR - 2015-04-25 02:01:37 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Users/hareeshudumbath/CIAssignments/ci_clean_version/system/database/drivers/mysql/mysql_driver.php 91
+DEBUG - 2015-04-25 02:01:37 --> Session Class Initialized
+DEBUG - 2015-04-25 02:01:37 --> Helper loaded: string_helper
+DEBUG - 2015-04-25 02:01:37 --> Session routines successfully run
+DEBUG - 2015-04-25 02:01:37 --> Controller Class Initialized
+DEBUG - 2015-04-25 02:01:37 --> Language file loaded: language/english/profiler_lang.php
+DEBUG - 2015-04-25 02:01:37 --> Helper loaded: text_helper
+DEBUG - 2015-04-25 02:01:37 --> Final output sent to browser
+DEBUG - 2015-04-25 02:01:37 --> Total execution time: 0.0270
+DEBUG - 2015-04-25 02:01:46 --> Config Class Initialized
+DEBUG - 2015-04-25 02:01:46 --> Hooks Class Initialized
+DEBUG - 2015-04-25 02:01:46 --> Utf8 Class Initialized
+DEBUG - 2015-04-25 02:01:46 --> UTF-8 Support Enabled
+DEBUG - 2015-04-25 02:01:46 --> URI Class Initialized
+DEBUG - 2015-04-25 02:01:46 --> Router Class Initialized
+DEBUG - 2015-04-25 02:01:46 --> Output Class Initialized
+DEBUG - 2015-04-25 02:01:46 --> Security Class Initialized
+DEBUG - 2015-04-25 02:01:46 --> Input Class Initialized
+DEBUG - 2015-04-25 02:01:46 --> Global POST and COOKIE data sanitized
+DEBUG - 2015-04-25 02:01:46 --> Language Class Initialized
+DEBUG - 2015-04-25 02:01:46 --> Loader Class Initialized
+DEBUG - 2015-04-25 02:01:46 --> Helper loaded: url_helper
+DEBUG - 2015-04-25 02:01:46 --> Database Driver Class Initialized
+ERROR - 2015-04-25 02:01:46 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Users/hareeshudumbath/CIAssignments/ci_clean_version/system/database/drivers/mysql/mysql_driver.php 91
+DEBUG - 2015-04-25 02:01:46 --> Session Class Initialized
+DEBUG - 2015-04-25 02:01:46 --> Helper loaded: string_helper
+DEBUG - 2015-04-25 02:01:46 --> Session routines successfully run
+DEBUG - 2015-04-25 02:01:46 --> Controller Class Initialized
+DEBUG - 2015-04-25 02:01:46 --> Language file loaded: language/english/profiler_lang.php
+DEBUG - 2015-04-25 02:01:46 --> Helper loaded: text_helper
+DEBUG - 2015-04-25 02:01:46 --> Final output sent to browser
+DEBUG - 2015-04-25 02:01:46 --> Total execution time: 0.0315
+DEBUG - 2015-04-25 02:02:10 --> Config Class Initialized
+DEBUG - 2015-04-25 02:02:10 --> Hooks Class Initialized
+DEBUG - 2015-04-25 02:02:10 --> Utf8 Class Initialized
+DEBUG - 2015-04-25 02:02:10 --> UTF-8 Support Enabled
+DEBUG - 2015-04-25 02:02:10 --> URI Class Initialized
+DEBUG - 2015-04-25 02:02:10 --> Router Class Initialized
+DEBUG - 2015-04-25 02:02:10 --> Output Class Initialized
+DEBUG - 2015-04-25 02:02:10 --> Security Class Initialized
+DEBUG - 2015-04-25 02:02:10 --> Input Class Initialized
+DEBUG - 2015-04-25 02:02:10 --> Global POST and COOKIE data sanitized
+DEBUG - 2015-04-25 02:02:10 --> Language Class Initialized
+DEBUG - 2015-04-25 02:02:10 --> Loader Class Initialized
+DEBUG - 2015-04-25 02:02:10 --> Helper loaded: url_helper
+DEBUG - 2015-04-25 02:02:10 --> Database Driver Class Initialized
+ERROR - 2015-04-25 02:02:10 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Users/hareeshudumbath/CIAssignments/ci_clean_version/system/database/drivers/mysql/mysql_driver.php 91
+DEBUG - 2015-04-25 02:02:10 --> Session Class Initialized
+DEBUG - 2015-04-25 02:02:10 --> Helper loaded: string_helper
+DEBUG - 2015-04-25 02:02:10 --> Session routines successfully run
+DEBUG - 2015-04-25 02:02:10 --> Controller Class Initialized
+DEBUG - 2015-04-25 02:02:10 --> Final output sent to browser
+DEBUG - 2015-04-25 02:02:10 --> Total execution time: 0.0311
+DEBUG - 2015-04-25 02:02:16 --> Config Class Initialized
+DEBUG - 2015-04-25 02:02:16 --> Hooks Class Initialized
+DEBUG - 2015-04-25 02:02:16 --> Utf8 Class Initialized
+DEBUG - 2015-04-25 02:02:16 --> UTF-8 Support Enabled
+DEBUG - 2015-04-25 02:02:16 --> URI Class Initialized
+DEBUG - 2015-04-25 02:02:16 --> Router Class Initialized
+ERROR - 2015-04-25 02:02:16 --> 404 Page Not Found --> login
+DEBUG - 2015-04-25 02:03:07 --> Config Class Initialized
+DEBUG - 2015-04-25 02:03:07 --> Hooks Class Initialized
+DEBUG - 2015-04-25 02:03:07 --> Utf8 Class Initialized
+DEBUG - 2015-04-25 02:03:07 --> UTF-8 Support Enabled
+DEBUG - 2015-04-25 02:03:07 --> URI Class Initialized
+DEBUG - 2015-04-25 02:03:07 --> Router Class Initialized
+ERROR - 2015-04-25 02:03:07 --> 404 Page Not Found --> login
+DEBUG - 2015-04-25 02:03:15 --> Config Class Initialized
+DEBUG - 2015-04-25 02:03:15 --> Hooks Class Initialized
+DEBUG - 2015-04-25 02:03:15 --> Utf8 Class Initialized
+DEBUG - 2015-04-25 02:03:15 --> UTF-8 Support Enabled
+DEBUG - 2015-04-25 02:03:15 --> URI Class Initialized
+DEBUG - 2015-04-25 02:03:15 --> Router Class Initialized
+DEBUG - 2015-04-25 02:03:15 --> Output Class Initialized
+DEBUG - 2015-04-25 02:03:15 --> Security Class Initialized
+DEBUG - 2015-04-25 02:03:15 --> Input Class Initialized
+DEBUG - 2015-04-25 02:03:15 --> Global POST and COOKIE data sanitized
+DEBUG - 2015-04-25 02:03:15 --> Language Class Initialized
+DEBUG - 2015-04-25 02:03:15 --> Loader Class Initialized
+DEBUG - 2015-04-25 02:03:15 --> Helper loaded: url_helper
+DEBUG - 2015-04-25 02:03:15 --> Database Driver Class Initialized
+ERROR - 2015-04-25 02:03:15 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Users/hareeshudumbath/CIAssignments/ci_clean_version/system/database/drivers/mysql/mysql_driver.php 91
+DEBUG - 2015-04-25 02:03:15 --> Session Class Initialized
+DEBUG - 2015-04-25 02:03:15 --> Helper loaded: string_helper
+DEBUG - 2015-04-25 02:03:15 --> Session routines successfully run
+DEBUG - 2015-04-25 02:03:15 --> Controller Class Initialized
+DEBUG - 2015-04-25 02:03:15 --> Final output sent to browser
+DEBUG - 2015-04-25 02:03:15 --> Total execution time: 0.0270
+DEBUG - 2015-04-25 02:03:22 --> Config Class Initialized
+DEBUG - 2015-04-25 02:03:22 --> Hooks Class Initialized
+DEBUG - 2015-04-25 02:03:22 --> Utf8 Class Initialized
+DEBUG - 2015-04-25 02:03:22 --> UTF-8 Support Enabled
+DEBUG - 2015-04-25 02:03:22 --> URI Class Initialized
+DEBUG - 2015-04-25 02:03:22 --> Router Class Initialized
+DEBUG - 2015-04-25 02:03:22 --> Output Class Initialized
+DEBUG - 2015-04-25 02:03:22 --> Security Class Initialized
+DEBUG - 2015-04-25 02:03:22 --> Input Class Initialized
+DEBUG - 2015-04-25 02:03:22 --> Global POST and COOKIE data sanitized
+DEBUG - 2015-04-25 02:03:22 --> Language Class Initialized
+DEBUG - 2015-04-25 02:03:22 --> Loader Class Initialized
+DEBUG - 2015-04-25 02:03:22 --> Helper loaded: url_helper
+DEBUG - 2015-04-25 02:03:22 --> Database Driver Class Initialized
+ERROR - 2015-04-25 02:03:22 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Users/hareeshudumbath/CIAssignments/ci_clean_version/system/database/drivers/mysql/mysql_driver.php 91
+DEBUG - 2015-04-25 02:03:22 --> Session Class Initialized
+DEBUG - 2015-04-25 02:03:22 --> Helper loaded: string_helper
+DEBUG - 2015-04-25 02:03:22 --> Session routines successfully run
+DEBUG - 2015-04-25 02:03:22 --> Controller Class Initialized
+DEBUG - 2015-04-25 02:03:22 --> Final output sent to browser
+DEBUG - 2015-04-25 02:03:22 --> Total execution time: 0.0268
