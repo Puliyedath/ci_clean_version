c60d9983f3fd3258a29723d9c581fae6c936806a
hareesh <hareesh.puliyedath@gmail.com>
2015-04-28 10:12:11 -0700

    with bootstrap ref

---
+++ application/views/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href= <?php echo base_url("assets/css/bootstrap.css"); ?>  type="text/css" media="screen" title="no title" charset="utf-8">
+        <title>tetst</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <ul class="col-md-5">
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                </ul>
+            </div>
+        </div>
+    </body>
+    <script type="text/javascript" src="<?= base_url("assets/js/jquery-2.1.3.min.js"); ?>" >
+
+    </script>
+    <script type="text/javascript" src="<?= base_url("assets/js/bootstarp.js"); ?>" >
+    </script>
+</html>
